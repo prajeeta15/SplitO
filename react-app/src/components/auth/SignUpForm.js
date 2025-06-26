8768a13b617741446c88a181859c4e250250240a
prajeeta pal <96904203+prajeeta15@users.noreply.github.com>
2025-06-26 20:49:23 +0530

    Update SignUpForm.js

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
-import background from '../../assets/background.png'
-import './auth.css'
-import footer from '../../assets/footer.png'
-import github from '../../assets/github.png'
+import background from '../../assets/background.png';
+import footer from '../../assets/footer.png';
+import './auth.css';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -13,171 +12,77 @@ const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
-
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [nickname, setNickname] = useState('')
+  const [nickname, setNickname] = useState('');
 
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
+    const errorsArr = [];
 
-    const errorsArr = []
+    if (firstName.length > 49) errorsArr.push("First Name must be less than 50 characters");
+    if (lastName.length > 49) errorsArr.push("Last Name must be less than 50 characters");
+    if (username.length > 39) errorsArr.push("Username must be less than 40 characters");
+    if (nickname.length > 49) errorsArr.push("Nickname must be less than 50 characters");
+    if (email.length > 254) errorsArr.push("Email must be less than 255 characters");
+    if (password.length > 30) errorsArr.push("Password must be less than 30 characters");
+    if (password !== repeatPassword) errorsArr.push("Passwords do not match");
 
-    if (firstName.length > 49) {
-      errorsArr.push("First Name must be less than 50 characters")
-    }
-    if (lastName.length > 49) {
-      errorsArr.push("Last Name must be less than 50 characters")
-    }
-    if (username.length > 39) {
-      errorsArr.push("Username must be less than 40 characters")
-    }
-    if (nickname.length > 49) {
-      errorsArr.push("Nickname must be less than 50 characters")
-    }
-    if (email.length > 254) {
-      errorsArr.push("Email length must be less than 255 characters")
-    }
-    if (password.length > 30) {
-      errorsArr.push("Password length must be less than 30 characters")
-    }
-    if (password !== repeatPassword){
-      errorsArr.push("Passwords do not match")
-    }
+    if (errorsArr.length) return setErrors(errorsArr);
 
-    if (errorsArr.length) {
-      setErrors(errorsArr)
-      return;
-    }
-
-
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password, firstName, lastName, nickname));
-      if (data) {
-        setErrors(data)
-      }
-    }
+    const data = await dispatch(signUp(username, email, password, firstName, lastName, nickname));
+    if (data) setErrors(data);
   };
 
-  const updateFirstName = (e) => {
-    setFirstName(e.target.value);
-  }
-
-  const updateLastName = (e) => {
-    setLastName(e.target.value);
-  }
-
-  const updateNickname = (e) => {
-    setNickname(e.target.value);
-  }
-
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
-
-  if (user) {
-    return <Redirect to='/dashboard' />;
-  }
+  if (user) return <Redirect to='/dashboard' />;
 
   return (
     <>
-      <div id='signup-form-container' style={{ backgroundImage: `url(${background})` }}>
-        <form id='signup-form' onSubmit={onSignUp}>
-          <div>
-            {errors.map((error, ind) => (
-              <div key={ind}>{error}</div>
-            ))}
+      <div className="auth-background" style={{ backgroundImage: `url(${background})` }}>
+        <form className="auth-form" onSubmit={onSignUp}>
+          <h2>Create Your Account</h2>
+          {errors.length > 0 && (
+            <ul className="auth-errors">
+              {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+          )}
+          <div className="form-grid">
+            <label>
+              First Name
+              <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required />
+            </label>
+            <label>
+              Last Name
+              <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} required />
+            </label>
+            <label>
+              Nickname
+              <input type="text" value={nickname} onChange={e => setNickname(e.target.value)} required />
+            </label>
+            <label>
+              Username
+              <input type="text" value={username} onChange={e => setUsername(e.target.value)} required />
+            </label>
+            <label>
+              Email
+              <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+            </label>
+            <label>
+              Password
+              <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+            </label>
+            <label>
+              Repeat Password
+              <input type="password" value={repeatPassword} onChange={e => setRepeatPassword(e.target.value)} required />
+            </label>
           </div>
-          <div className='signup_form_input_container'>
-            <div>First Name</div>
-            <input
-              type='text'
-              name='First Name'
-              onChange={updateFirstName}
-              value={firstName}
-              required={true}
-            ></input>
-          </div>
-          <div className='signup_form_input_container'>
-            <div>Last Name</div>
-            <input
-              type='text'
-              name='Last Name'
-              onChange={updateLastName}
-              value={lastName}
-              required={true}
-            ></input>
-          </div>
-          <div className='signup_form_input_container'>
-            <div>Nickname</div>
-            <input
-              type='text'
-              name='Nickname'
-              onChange={updateNickname}
-              value={nickname}
-              required={true}
-            ></input>
-          </div>
-          <div className='signup_form_input_container'>
-            <div>User Name</div>
-            <input
-              type='text'
-              name='username'
-              onChange={updateUsername}
-              value={username}
-              required={true}
-            ></input>
-          </div>
-          <div className='signup_form_input_container'>
-            <div>Email</div>
-            <input
-              type='email'
-              name='email'
-              onChange={updateEmail}
-              value={email}
-              required={true}
-            ></input>
-          </div>
-          <div className='signup_form_input_container'>
-            <div>Password</div>
-            <input
-              type='password'
-              name='password'
-              onChange={updatePassword}
-              value={password}
-              required={true}
-            ></input>
-          </div>
-          <div className='signup_form_input_container'>
-            <div>Repeat Password</div>
-            <input
-              type='password'
-              name='repeat_password'
-              onChange={updateRepeatPassword}
-              value={repeatPassword}
-              required={true}
-            ></input>
-          </div>
-          <button id='signup-form-button' type='submit'>Sign Up</button>
+          <button type="submit" className="auth-submit-btn">Sign Up</button>
         </form>
       </div>
-      
-      <img className='footer' src={footer} alt='footer'></img>
+      <img className="footer" src={footer} alt="footer" />
     </>
   );
 };
