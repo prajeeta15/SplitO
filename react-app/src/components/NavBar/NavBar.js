7a80ff142d901f0ef9cded1835762f282bd8cc2f
prajeeta pal <96904203+prajeeta15@users.noreply.github.com>
2025-06-26 21:00:18 +0530

    Update NavBar.js

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
-import logo from '../../assets/logo.png'; // Replace with your actual logo file
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <NavLink to="/" className="navbar-logo">
-          <img src={logo} alt="logo" />
           <span>Smartsplit</span>
         </NavLink>
       </div>
