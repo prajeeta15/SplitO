cca894d189e1d08d862066d4e72d79934ce0fbf2
prajeeta pal <96904203+prajeeta15@users.noreply.github.com>
2025-06-26 20:06:18 +0530

    Update api.js

--- react-app/src/api.js
+++ react-app/src/api.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://splito.onrender.com"
+    ? process.env.REACT_APP_BASE_URL
     : "http://localhost:5000"; // dev mode
 
 const getCSRFToken = async () => {
